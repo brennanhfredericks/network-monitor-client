83d6ddf5a281e646546452500a7a2c724b9e92a9
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-03-24 12:52:59 +0200

    added service_manager to manager thread and provide access to address family properties

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 
 # TODO
-    - implement commandline interface using argparse with following options
-      - list all interfaces
-      - list all gateways
-      - specify interface using -interface 'en0'
-      - additional:
+    - Add services class to start and stop threads used to retrieve and process information
+    - use queues to pass information between threads
+
+# Implemented
+    - basic command line interface implemented with the following argument options
+      - list all interfaces (-li, --list-interfaces)
+      - list all gateways (-lg, --list-gateways)
+      - specify interface to monitor (-i, --interfaces) with interface name
+
+# Possible Additional Features
+## Starting Commandline Options
+    - additional commads:
         - set server address
         - generate empty config file
         - start using config file
-   
-
-# Features
-## Starting Commandline Options
-    - add commad line arguments to list all interfaces, gateways using netifaces
-    - e.g. commands:
-        - network_monitor -interface 'enp0s3'
-        - network_monitor -list-interfaces , -list-gateways
-
-
-
+        - add functionality to listen on multiple interfaces
 - use interface addresses as identifier when posting data to server
 
 
--- network_monitor.py
+++ network_monitor.py
@@ -2,31 +2,91 @@ import netifaces
 import argparse
 
 
+class Service_Manager:
+    def __init__(self, interface_name):
+        self._ifaddress = netifaces.ifaddresses(interface_name)
+
+    @property
+    def interface_addresses(self):
+        return self._ifaddress
+
+    @property
+    def address_family(self):
+        return list(self._ifaddress.keys())
+
+    @property
+    def ipv4_address(self):
+
+        if netifaces.AF_INET in self._ifaddress.keys():
+            return self._ifaddress[netifaces.AF_INET]
+
+        return None
+
+    @property
+    def ipv6_address(self):
+
+        if netifaces.AF_INET6 in self._ifaddress.keys():
+            return self._ifaddress[netifaces.AF_INET6]
+
+        return None
+
+    @property
+    def link_layer_address(self):
+
+        if netifaces.AF_LINK in self._ifaddress.keys():
+            return self._ifaddress[netifaces.AF_LINK]
+
+        return None
+
 def main():
 
-    basic_parser = argparse.ArgumentParser(description="monitor ethernet network packets.",add_help=True)
+    basic_parser = argparse.ArgumentParser(
+        description="monitor ethernet network packets.", add_help=True
+    )
 
     # add arguments
-    basic_parser.add_argument('-i','--interface',action='store',choices=netifaces.interfaces(),type=str,help= 'specify which interface to monitor')
-    basic_parser.add_argument('-li','--list-interfaces',action='store_true',help='list all available interfaces')
-    basic_parser.add_argument('-lg','--list-gateways',action='store_true',help='list all available gateways')
-    
+    basic_parser.add_argument(
+        "-i",
+        "--interface",
+        action="store",
+        choices=netifaces.interfaces(),
+        type=str,
+        help="specify which interface to monitor",
+    )
+    basic_parser.add_argument(
+        "-li",
+        "--list-interfaces",
+        action="store_true",
+        help="list all available interfaces",
+    )
+    basic_parser.add_argument(
+        "-lg",
+        "--list-gateways",
+        action="store_true",
+        help="list all available gateways",
+    )
+
     # parse arguments
     args = basic_parser.parse_args()
 
     if args.interface is not None:
         # check validate choice and start process
-        pass
+        service_manager = Service_Manager(args.interface)
+        print(service_manager.interface_addresses)
+        print(service_manager.address_family)
+        print(service_manager.ipv4_address)
+        print(service_manager.ipv6_address)
+        print(service_manager.link_layer_address)
     else:
 
         if args.list_gateways:
             print("gateways: ")
-            for k,v in netifaces.gateways().items():
+            for k, v in netifaces.gateways().items():
                 print(f"\taddress family: {k}, interface: {v}")
-        
+
         if args.list_interfaces:
             print(f"interfaces: {netifaces.interfaces()}")
-   
+
 
 if __name__ == "__main__":
     exit(main())
