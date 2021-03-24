11561446bf4396155af51b05be90239d93f06323
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-03-24 11:59:42 +0200

    implemented basic command line arguments

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# Starting Commandline Options
+
+# TODO
+    - implement commandline interface using argparse with following options
+      - list all interfaces
+      - list all gateways
+      - specify interface using -interface 'en0'
+      - additional:
+        - set server address
+        - generate empty config file
+        - start using config file
+   
+
+# Features
+## Starting Commandline Options
     - add commad line arguments to list all interfaces, gateways using netifaces
     - e.g. commands:
         - network_monitor -interface 'enp0s3'
@@ -9,13 +22,13 @@
 - use interface addresses as identifier when posting data to server
 
 
-# Network Listener
+## Network Listener
     - look up os and create low level socket. requires sudo permission
     - keep listing until stopped
 
-# Packet Processer
+## Packet Processer
     - parse raw packet into format and drop data
 
-# Reporter
+## Reporter
     - send data to server, if not available log to sqlite database
     - when server is available transfer sqlite database to monitor server 
--- network_monitor.py
+++ network_monitor.py
@@ -1,5 +1,32 @@
 import netifaces
+import argparse
 
-print(netifaces.gateways())
-#print(netifaces.ifaddresses())
-print(netifaces.interfaces())
+
+def main():
+
+    basic_parser = argparse.ArgumentParser(description="monitor ethernet network packets.",add_help=True)
+
+    # add arguments
+    basic_parser.add_argument('-i','--interface',action='store',choices=netifaces.interfaces(),type=str,help= 'specify which interface to monitor')
+    basic_parser.add_argument('-li','--list-interfaces',action='store_true',help='list all available interfaces')
+    basic_parser.add_argument('-lg','--list-gateways',action='store_true',help='list all available gateways')
+    
+    # parse arguments
+    args = basic_parser.parse_args()
+
+    if args.interface is not None:
+        # check validate choice and start process
+        pass
+    else:
+
+        if args.list_gateways:
+            print("gateways: ")
+            for k,v in netifaces.gateways().items():
+                print(f"\taddress family: {k}, interface: {v}")
+        
+        if args.list_interfaces:
+            print(f"interfaces: {netifaces.interfaces()}")
+   
+
+if __name__ == "__main__":
+    exit(main())
