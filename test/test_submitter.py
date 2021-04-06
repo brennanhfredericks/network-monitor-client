7ef30db0c88ac4101f3942d7ac5917e287bb885b
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-04-06 22:38:04 +0200

    added submitter log file test

--- test/test_submitter.py
+++ test/test_submitter.py
@@ -7,6 +7,7 @@ import threading
 import binascii
 import os
 import time
+import json
 
 from network_monitor.protocols import (
     Packet_802_2,
@@ -16,6 +17,54 @@ from network_monitor.protocols import (
 from network_monitor import Packet_Submitter, Packet_Filter, Filter
 from network_monitor.filters import present_protocols
 from test_load_data import load_filev2
+
+
+def submitter_logged_file():
+    feeder = queue.Queue()
+
+    t0_filter = Filter(
+        "test0",
+        {"AF_Packet": {"ifname": "lo"}},
+    )
+
+    packet_filter = Packet_Filter()
+    packet_filter.register(t0_filter)
+
+    packet_submitter = Packet_Submitter(feeder)
+
+    packet_submitter.start()
+    for i, (af_packet, raw_packet) in enumerate(
+        load_filev2(
+            "raw2_protocols_1617213286_IPv4_IPv6_UDP_TCP_ARP_ICMP_ICMPv6_IGMP_LLDP_CDP.lp",
+            log_dir="./remote_data",
+        )
+    ):
+
+        if af_packet["protocol"] > 1500:
+            out = Packet_802_3(raw_packet)
+        else:
+            out = Packet_802_2(raw_packet)
+
+        res = packet_filter.apply(af_packet, out)
+
+        if res is None:
+            continue
+        else:
+            feeder.put(res)
+
+    packet_submitter.stop()
+
+    with open(packet_submitter._submitter.out_file, "r") as fin:
+        for line in fin:
+            data = json.loads(line)
+
+            res = t0_filter.apply(data)
+            assert res == False
+
+
+def test_submitter_logger():
+
+    submitter_logged_file()
 
 
 def start_submitter():
@@ -55,7 +104,6 @@ def start_submitter():
     assert True
 
 
-def test_submitter():
+def test_submitter_useless():
 
-    # start_submitter()
-    ...
+    start_submitter()
