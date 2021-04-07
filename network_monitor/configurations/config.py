a7f8bdfe9c88aaf8a93e193b5058c6795848a21c
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-04-07 20:28:31 +0200

    generate config file updated

--- network_monitor/configurations/config.py
+++ network_monitor/configurations/config.py
@@ -9,6 +9,34 @@ def generate_template_configuration(config_name: str):
     # create config file
     config = configparser.ConfigParser()
 
+    # template example
+    template = """
+# Specify interface on which the ethernet listener service should be started.
+# Requires super user priviliages to change the interface to operate in promiscous
+[ListenerService]
+# InterfaceName = "enp0s3"
+
+# Specify application settings. global settings some of which affect other services.
+[Application]
+# Log = "./logs/application/general/"
+# UnknownProtocols = "./logs/application/unknown_protocols"
+# FilterAllApplicationTraffic = True
+
+# Specify submitter service setting.
+[SubmitterService]
+# Local = "./logs/submitter_service/"
+# Url = "http://127.0.0.1:5000/packets"
+# RetryInterval = 300
+
+# configure filter settings
+[Filter]
+# Definition = {"AF_Packet":{"ifname":"lo"},"TCP":{}}
+
+# additional config string
+[Filter1]
+# Definition = {"ICMP":{}}
+    """
+
     # defualt configuration variables use for development
     config["DEFAULT"] = {
         "InterfaceName": "enp0s3",
@@ -23,29 +51,7 @@ def generate_template_configuration(config_name: str):
         },
     }
 
-    with open(os.path.join("./", f"{config_name}.cfg"), "w") as fout:
+    with open(os.path.join("./", f"{config_name}"), "w") as fout:
         config.write(fout)
 
-    # Specify interface on which the ethernet listener service should be started.
-    # Requires super user priviliages to change the interface to operate in promiscous
-    # [ListenerService]
-    # InterfaceName = "enp0s3"
-
-    # Specify application settings. global settings some of which affect other services.
-    # [Application]
-    # Log = "./logs/application/general/"
-    # UnknownProtocols = "./logs/application/unknown_protocols"
-    # FilterAllApplicationTraffic = True
-
-    # Specify submitter service setting.
-    # [SubmitterService]
-    # Local = "./logs/submitter_service/"
-    # Url = "http://127.0.0.1:5000/packets"
-    # RetryInterval = 300
-
-    # configure filter settings
-    # [Filter]
-    # Definition = {"AF_Packet":{"ifname":"lo"},"TCP":{}}
-
-    # [Filter]
-    # Definition = {"ICMP":{}}
+        fout.write(template)
