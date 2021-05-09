42845ac1ad4d102f92c00a53403249e34dfe8a8c
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-05-09 20:07:26 +0200

    added annotations internet layer

--- network_monitor/protocols/internet_layer.py
+++ network_monitor/protocols/internet_layer.py
@@ -3,9 +3,11 @@ import struct
 import base64
 import json
 import dataclasses
+import asyncio
+
 from dataclasses import dataclass
-
-
+from typing import Union, Dict, Any, List, Tuple, Optional
+from aiologger import Logger
 from .protocol_utils import (
     get_ipv4_addr,
     get_ipv6_addr,
@@ -39,7 +41,7 @@ class IPv4(object):
     Destination_Address: str
     Options: dict
 
-    def __init__(self, raw_bytes):
+    def __init__(self, raw_bytes: bytes) -> None:
 
         (
             __vihl,
@@ -55,71 +57,47 @@ class IPv4(object):
         ) = struct.unpack("! B B H H H B B H 4s 4s", raw_bytes[:20])
 
         __flags = __ff & 57344 >> 13
-        self.version = __vihl >> 4
+        self.Version: int = __vihl >> 4
 
-        self.IHL = __vihl & 15
+        self.IHL: int = __vihl & 15
         self.__verify_checksum(raw_bytes[: self.IHL * 4])
-        self.DSCP = (__dsen & 252) >> 2
-        self.ECN = __dsen & 3
-        self.total_length = __tl
-        self.identification = __ident
+        self.DSCP: int = (__dsen & 252) >> 2
+        self.ECN: int = __dsen & 3
+        self.Total_Length: int = __tl
+        self.Identification: int = __ident
 
-        self.flags = __flags
-        self.fragment_offset = __ff & 8191
-        self.TTL = __ttl
-        self.protocol = __proto
-        self.header_checksum = __hck
-        self.source_address = get_ipv4_addr(__src)
-        self.destination_address = get_ipv4_addr(__des)
+        self.Flags: int = __flags
+        self.Fragment_Offset: int = __ff & 8191
+        self.TTL: int = __ttl
+        self.Protocol: int = __proto
+        self.Header_Checksum: int = __hck
+        self.Source_Address: str = get_ipv4_addr(__src)
+        self.Destination_Address: str = get_ipv4_addr(__des)
 
         # Note: If the header length is greater than 5 (i.e., it is from 6 to 15)
         # it means that the options field is present and must be considered.
 
         if self.IHL > 5:
             # raw bytes contains Option field data
-            offset = 5 * self.IHL  # 8*4 24 bytes
-            options = raw_bytes[20:offset]
+            offset: int = 5 * self.IHL  # 8*4 24 bytes
+            options: bytes = raw_bytes[20:offset]
 
             self.__parse_options(options)
             self.__parse_upper_layer_protocol(raw_bytes[offset:])
         else:
             self.__parse_upper_layer_protocol(raw_bytes[20:])
-            self.options = {}
+            self.Options = {}
 
-        self.__raw_bytes = raw_bytes
+        self.__raw_bytes: bytes = raw_bytes
 
-    def __verify_checksum(self, raw_bytes_header: bytes):
+    def __verify_checksum(self, raw_bytes_header: bytes) -> None:
         """ verify checksum is correct """
 
         # https://www.thegeekstuff.com/2012/05/ip-header-checksum/
-        checksum = 0
-        read_checksum = 0
-        int16s = []
-        (raw_bytes_header,) = struct.unpack(
-            f"! {len(raw_bytes_header)}s", raw_bytes_header
-        )
-        for i, (high, low) in enumerate(grouper(raw_bytes_header, 2, fillvalue=0)):
 
-            if i == 5:
-                read_checksum = own = f"{(high << 8)+low:x}"
-                read_checksum = int(read_checksum, 16)
+        raise NotImplemented
 
-                int16s.append(0)
-                # print(f"0x{int16:04x} ", end="")
-
-            else:
-                own = f"{(high << 8)+low:x}"
-                int16 = int(own, 16)
-                int16s.append(int16)
-                # print(f"0x{int16:x} ", end="")
-        checksum = int16s[0]
-        for i in range(1, len(int16s)):
-            checksum = ones_comp_add16(checksum, int16s[i])
-        # print(f" result: {checksum}")
-
-    # print(f"0x{raw_bytes_header:2x}")
-
-    def __parse_options(self, options: bytes):
+    def __parse_options(self, options: bytes) -> None:
         """ used to parser Options flield """
         # Note: Copied, Option Class, and Option Number are sometimes referred to as a single eight-bit field, the Option Type.
         __ccn, __length = struct.unpack("! B B", options[:2])
@@ -127,26 +105,26 @@ class IPv4(object):
         copied = __ccn >> 7
         klass = (__ccn & 96) >> 5
         number = __ccn & 31
-        self.options = {
+        self.Options: Dict[str, Union[str, int]] = {
             "Copied": copied,
             "Option Class": klass,
             "Option Number": number,
             "Option Data": base64.b64encode(__data).decode("utf-8"),
         }
 
-    def raw(self):
+    def raw(self) -> bytes:
         return self.__raw_bytes
 
-    def upper_layer(self):
+    def upper_layer(self) -> Any:
 
         return self.__encap
 
-    def serialize(self):
+    def serialize(self) -> Dict[str, Union[str, int]]:
         return dataclasses.asdict(self)
 
-    def __parse_upper_layer_protocol(self, remaining_raw_bytes):
+    def __parse_upper_layer_protocol(self, remaining_raw_bytes: bytes):
 
-        self.__encap = Protocol_Parser.parse(
+        self.__encap: Any = Protocol_Parser.parse(
             Layer_Protocols.IP_protocols, self.protocol, remaining_raw_bytes
         )
 
@@ -172,11 +150,11 @@ class IPv6_Ext_Headers(object):
         254,  #: "Reserved",
     ]
 
-    def __init__(self):
-        self._headers = []
+    def __init__(self) -> None:
+        self._headers: List[Any, ...] = []
 
-    def parse_extension_headers(self, next_header, raw_bytes):
-        def parse_header(remaining_raw_bytes):
+    def parse_extension_headers(self, next_header: int, raw_bytes: bytes) -> Tuple[Any, int, bytes]:
+        def parse_header(remaining_raw_bytes: bytes) -> Tuple[int, bytes, bytes]:
             __next_header, __ext_header_len = struct.unpack(
                 "! B B", remaining_raw_bytes[:2]
             )
@@ -213,20 +191,20 @@ class IPv6_Ext_Headers(object):
 @dataclass
 class IPv6(object):
 
-    description = "Internet Protocol Version 6"
-    identifier = 34525
-    version: int
+    Description = "Internet Protocol Version 6"
+    Identifier = 34525
+    Version: int
     DS: int
     ECN: int
-    flow_label: int
-    payload_length: int
-    next_header: int
-    hop_limit: int
-    source_address: str
-    destination_address: str
-    ext_headers: list
+    Flow_Label: int
+    Payload_Length: int
+    Next_Header: int
+    Hop_Limit: int
+    Source_Address: str
+    Destination_Address: str
+    Ext_Headers: list
 
-    def __init__(self, raw_bytes):
+    def __init__(self, raw_bytes: bytes) -> None:
 
         # parse fixed header
         __vtf, __pl_len, __next_h, __hop_l, __src_addr, __des_addr = struct.unpack(
@@ -234,43 +212,44 @@ class IPv6(object):
         )
 
         # index first byte
-        self.version = __vtf[0] >> 4
+        self.Version: int = __vtf[0] >> 4
         traffic_class = (int.from_bytes(__vtf[:2], sys.byteorder) & 2040) >> 4
-        self.DS = traffic_class & 252
-        self.ECN = traffic_class & 3
+        self.DS: int = traffic_class & 252
+        self.ECN: int = traffic_class & 3
 
-        self.flow_label = int.from_bytes(__vtf[1:4], sys.byteorder) & 1048575
-        self.payload_length = __pl_len
-        self.next_header = __next_h
-        self.hop_limit = __hop_l
-        self.source_address = get_ipv6_addr(__src_addr)
-        self.destination_address = get_ipv6_addr(__des_addr)
+        self.Flow_Label: int = int.from_bytes(
+            __vtf[1:4], sys.byteorder) & 1048575
+        self.Payload_Length: int = __pl_len
+        self.Next_Header: int = __next_h
+        self.Hop_Limit: int = __hop_l
+        self.Source_Address: str = get_ipv6_addr(__src_addr)
+        self.Destination_Address: str = get_ipv6_addr(__des_addr)
 
         # parse extension headers
         (
-            self.ext_headers,
+            self.Ext_Headers,
             protocol,
             remaining_raw_bytes,
         ) = IPv6_Ext_Headers().parse_extension_headers(self.next_header, raw_bytes[40:])
 
-        self._raw_bytes = raw_bytes
+        self._raw_bytes: bytes = raw_bytes
 
         # parse upper layer protocol
         self.__parse_upper_layer_protocol(protocol, remaining_raw_bytes)
 
-    def raw(self):
+    def raw(self) -> bytes:
         return self._raw_bytes
 
-    def upper_layer(self):
+    def upper_layer(self) -> Any:
         return self.__encap
 
-    def serialize(self):
+    def serialize(self) -> Dict[str, Union[str, int]]:
         return dataclasses.asdict(self)
 
-    def __parse_upper_layer_protocol(self, protocol, remaining_raw_bytes):
+    def __parse_upper_layer_protocol(self, protocol, remaining_raw_bytes: bytes) -> None:
         # The values are shared with those used for the IPv4 protocol field
 
-        self.__encap = Protocol_Parser.parse(
+        self.__encap: Any = Protocol_Parser.parse(
             Layer_Protocols.IP_protocols, protocol, remaining_raw_bytes
         )
 
@@ -287,13 +266,13 @@ class ARP(object):
     PTYPE: int
     HLEN: int
     PLEN: int
-    operation: int
+    Operation: int
     SHA: str
     SPA: str
     THA: str
     TPA: str
 
-    def __init__(self, raw_bytes):
+    def __init__(self, raw_bytes: bytes) -> None:
         (
             __hardware_type,
             __protocol_type,
@@ -306,38 +285,38 @@ class ARP(object):
             __target_proto_addr,
         ) = struct.unpack("! H H B B H 6s 4s 6s 4s", raw_bytes[:28])
 
-        self.HTYPE = __hardware_type
-        self.PTYPE = __protocol_type
-        self.HLEN = __hlen
-        self.PLEN = __plen
-        self.operation = __op_code
-        self.SHA = get_mac_addr(__sender_hw_addr)
-        self.SPA = self._decode_protocol_addr(__sender_proto_addr)
-        self.THA = get_mac_addr(__target_hw_addr)
-        self.TPA = self._decode_protocol_addr(__target_proto_addr)
+        self.HTYPE: int = __hardware_type
+        self.PTYPE: int = __protocol_type
+        self.HLEN: int = __hlen
+        self.PLEN: int = __plen
+        self.Operation: int = __op_code
+        self.SHA: str = get_mac_addr(__sender_hw_addr)
+        self.SPA: str = self._decode_protocol_addr(__sender_proto_addr)
+        self.THA: str = get_mac_addr(__target_hw_addr)
+        self.TPA: str = self._decode_protocol_addr(__target_proto_addr)
 
-        self._raw_bytes = raw_bytes
+        self._raw_bytes: bytes = raw_bytes
 
-    def raw(self):
+    def raw(self) -> bytes:
         return self._raw_bytes
 
-    def upper_layer(self):
+    def upper_layer(self) -> Optional[Any]:
         return None
 
-    def serialize(self):
+    def serialize(self) -> Dict[str, Union[str, int]]:
         return dataclasses.asdict(self)
 
-    def _decode_protocol_addr(self, proto_addr):
-
+    def _decode_protocol_addr(self, proto_addr: int) -> str:
+        logger = Logger.with_default_handlers()
         if self.PTYPE == 2048:
             return get_ipv4_addr(proto_addr)
         elif self.PTYPE == 34525:
             return get_ipv6_addr(proto_addr)
         else:
             # add logging functionality here
-            print(
+            asyncio.run(logger.warning(
                 f" address ARP decoding not implemented for {self.PTYPE}, address {proto_addr}"
-            )
+            ))
             return str(proto_addr)
 
 
@@ -346,20 +325,20 @@ Protocol_Parser.register(Layer_Protocols.Ethertype, 2054, ARP)
 
 @dataclass
 class CDP(object):
-    description = "Cisco Discovery Protocol"
-    identifier = 8192
+    Description = "Cisco Discovery Protocol"
+    Identifier = 8192
 
-    def __init__(self, raw_bytes):
+    def __init__(self, raw_bytes: bytes) -> None:
         # proprietary protocol
-        self._raw_bytes = raw_bytes
+        self._raw_bytes: bytes = raw_bytes
 
-    def raw(self):
+    def raw(self) -> bytes:
         return self._raw_bytes
 
-    def serialize(self):
+    def serialize(self) -> Dict[str, Union[str, int]]:
         return dataclasses.asdict(self)
 
-    def upper_layer(self):
+    def upper_layer(self) -> Optional[Any]:
         return None
 
 
@@ -369,23 +348,22 @@ Protocol_Parser.register(Layer_Protocols.Ethertype, 8192, CDP)
 @dataclass
 class LLDP(object):
 
-    description = "35020 IEEE Std 802.1AB - Link Layer Discovery Protocol"
-    identifier = 35020
-    typelengthvalue: list
+    Description = "35020 IEEE Std 802.1AB - Link Layer Discovery Protocol"
+    Identifier = 35020
+    TLV: list
 
-    def __init__(self, raw_bytes):
+    def __init__(self, raw_bytes: bytes) -> None:
 
-        #
         tlv_dict, r_bytes = self.__parse_tlv(raw_bytes)
-        self.typelengthvalue = [tlv_dict]
+        self.TLV: List[Dict[str, Union[str, int]]] = [tlv_dict]
         while tlv_dict["Type"] != 0:
             tlv_dict, r_bytes = self.__parse_tlv(r_bytes)
 
-            self.typelengthvalue.append(tlv_dict)
+            self.TLV.append(tlv_dict)
 
-        self._raw_bytes = raw_bytes
+        self._raw_bytes: bytes = raw_bytes
 
-    def __parse_tlv(self, raw_bytes):
+    def __parse_tlv(self, raw_bytes: bytes) -> Dict[str, Union[str, int]]:
         (__tl,) = struct.unpack("! H", raw_bytes[:2])
         type_ = (__tl & 0b1111111000000000) >> 9
         length = __tl & 0b111111111
@@ -397,13 +375,13 @@ class LLDP(object):
             "Value": base64.b64encode(value).decode("utf-8"),
         }, raw_bytes[length:]
 
-    def raw(self):
+    def raw(self) -> bytes:
         return self._raw_bytes
 
-    def upper_layer(self):
+    def upper_layer(self) -> Optional[Any]:
         return None
 
-    def serialize(self):
+    def serialize(self) -> Dict[str, Union[str, int]]:
         return dataclasses.asdict(self)
 
 
@@ -412,23 +390,23 @@ Protocol_Parser.register(Layer_Protocols.Ethertype, 35020, LLDP)
 
 @dataclass
 class Xerox(object):
-    description = "Xerox Experimental"
-    identifier = 103
-    message: str
+    Description = "Xerox Experimental"
+    Identifier = 103
+    Message: str
 
-    def __init__(self, raw_bytes):
+    def __init__(self, raw_bytes: bytes) -> None:
         (__msg,) = struct.unpack(f"! {len(raw_bytes)}s", raw_bytes)
 
-        self.message = base64.b64encode(__msg).decode("utf-8")
-        self._raw_bytes = raw_bytes
+        self.Message: str = base64.b64encode(__msg).decode("utf-8")
+        self._raw_bytes: bytes = raw_bytes
 
-    def raw(self):
+    def raw(self) -> bytes:
         return self._raw_bytes
 
-    def upper_layer(self):
+    def upper_layer(self) -> Optional[Any]:
         return None
 
-    def serialize(self):
+    def serialize(self) -> Dict[str, Union[str, int]]:
         return dataclasses.asdict(self)
 
 
@@ -438,31 +416,31 @@ Protocol_Parser.register(Layer_Protocols.Ethertype, 103, Xerox)
 @dataclass
 class IGMP(object):
 
-    description = "Internet Group Management Protocol"
-    identifier = 2
-    type_: int
-    max_resp_time: int
-    checksum: int
-    group_address: str
+    Description = "Internet Group Management Protocol"
+    Identifier = 2
+    Type: int
+    Max_Response_Time: int
+    Checksum: int
+    Group_Address: str
 
-    def __init__(self, raw_bytes):
+    def __init__(self, raw_bytes: bytes) -> None:
         __tp, __mrt, __chk, __ga = struct.unpack("! B B H 4s", raw_bytes[:8])
 
-        self.type_ = __tp
-        self.max_resp_time = __mrt
-        self.checksum = __chk
-        self.group_address = get_ipv4_addr(__ga)
+        self.Type: int = __tp
+        self.Max_Response_Time: int = __mrt
+        self.Checksum: int = __chk
+        self.Group_Address: str = get_ipv4_addr(__ga)
 
-        self._raw_bytes = raw_bytes
+        self._raw_bytes: bytes = raw_bytes
         # need to implement parser for message types
 
-    def raw(self):
+    def raw(self) -> bytes:
         return self._raw_bytes
 
-    def upper_layer(self):
+    def upper_layer(self) -> Optional[Any]:
         return None
 
-    def serialize(self):
+    def serialize(self) -> Dict[str, Union[str, int]]:
         return dataclasses.asdict(self)
 
 
@@ -472,28 +450,28 @@ Protocol_Parser.register(Layer_Protocols.IP_protocols, 2, IGMP)
 @dataclass
 class ICMPv6(object):
 
-    description = "Internet Control Message Protocol for IPv6"
-    identifier = 58
-    type_: int
-    code: int
-    checksum: int
-    message: bytes
+    Description = "Internet Control Message Protocol for IPv6"
+    Identifier = 58
+    Type: int
+    Code: int
+    Checksum: int
+    Message: str
 
-    def __init__(self, raw_bytes):
+    def __init__(self, raw_bytes: bytes) -> None:
         __tp, __cd, __chk, __msg = struct.unpack("! B B H 4s", raw_bytes[:8])
-        self.type_ = __tp
-        self.code = __cd
-        self.checksum = __chk
-        self.message = base64.b64encode(__msg).decode("utf-8")
-        self._raw_bytes = raw_bytes
+        self.Type: int = __tp
+        self.Code: int = __cd
+        self.Checksum: int = __chk
+        self.Message: str = base64.b64encode(__msg).decode("utf-8")
+        self._raw_bytes: bytes = raw_bytes
 
-    def raw(self):
+    def raw(self) -> bytes:
         return self._raw_bytes
 
-    def upper_layer(self):
+    def upper_layer(self) -> Optional[Any]:
         return None
 
-    def serialize(self):
+    def serialize(self) -> Dict[str, Union[str, int]]:
         return dataclasses.asdict(self)
 
 
@@ -503,31 +481,31 @@ Protocol_Parser.register(Layer_Protocols.IP_protocols, 58, ICMPv6)
 @dataclass
 class ICMP(object):
 
-    description = "Internet Control Message Protocol"
-    identifier = 1
-    type_: int
-    code: int
-    checksum: int
-    message: bytes
+    Description = "Internet Control Message Protocol"
+    Identifier = 1
+    Type: int
+    Code: int
+    Checksum: int
+    Message: str
 
-    def __init__(self, raw_bytes):
+    def __init__(self, raw_bytes: bytes) -> None:
 
         __tp, __cd, __chk, __msg = struct.unpack("! B B H 4s", raw_bytes[:8])
-        self.type_ = __tp
-        self.code = __cd
-        self.checksum = __chk
+        self.Type: int = __tp
+        self.Code: int = __cd
+        self.Checksum: int = __chk
 
         # implement parser to decode control messages
-        self.message = base64.b64encode(__msg).decode("utf-8")
-        self._raw_bytes = raw_bytes
+        self.Message = base64.b64encode(__msg).decode("utf-8")
+        self._raw_bytes: bytes = raw_bytes
 
-    def raw(self):
+    def raw(self) -> bytes:
         return self._raw_bytes
 
-    def upper_layer(self):
+    def upper_layer(self) -> Optional[Any]:
         return None
 
-    def serialize(self):
+    def serialize(self) -> Dict[str, Union[str, int]]:
         return dataclasses.asdict(self)
 
 
