bdf38c9c6b7f4d9e31191d7fd475a5e1760fea75
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-05-10 21:18:23 +0200

    annotated parsers

--- network_monitor/protocols/parsers.py
+++ network_monitor/protocols/parsers.py
@@ -2,6 +2,7 @@ import os
 import time
 import binascii
 
+from typing import Dict, Union, Any
 from functools import lru_cache
 
 from .protocol_utils import Unknown
@@ -12,37 +13,34 @@ from .layer import Layer_Protocols
 class Parser:
     """ class to register all packet parsers for various levels"""
 
-    __protocol_parsers = {}
-    __protocol_str_lookup = {}
+    __protocol_parsers: Dict[int, Dict[int, Any]] = {}
+    __protocol_str_lookup: Dict[str, Any] = {}
 
-    def __init__(self):
+    def __init__(self) -> None:
 
         # init layer protocols
         for layer_protocol in Layer_Protocols:
             self.__protocol_parsers[layer_protocol] = {}
 
-        self.__fname = f"raw_unknown_protocols_{int(time.time())}.lp"
+        self.__fname: str = f"raw_unknown_protocols_{int(time.time())}.lp"
 
     @property
-    def parsers(self):
+    def parsers(self) -> Dict[int, Dict[int, Any]]:
 
         return self.__protocol_parsers
 
-    def set_log_directory(self, log_dir):
+    def set_log_directory(self, log_dir: str) -> None:
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
 
-        self.__log = log_dir
+        self.__log: str = log_dir
 
-    def register(self, layer, identifier, protocol_parser):
+    def register(self, layer: int, identifier: int, protocol_parser: Any):
         # check if dataclass and callable
         self.__protocol_parsers[layer][identifier] = protocol_parser
         self.__protocol_str_lookup[protocol_parser.__name__] = protocol_parser
 
-    def _register_protocol_class_name(self, class_name, protocol_parser):
-        self.__protocol_str_lookup[class_name] = protocol_parser
-
-    def get_protocol_class_by_name(self, class_name: str):
+    def get_protocol_class_by_name(self, class_name: str) -> Any:
         """ return an empty protocol class used in comparison """
         try:
             res = self.__protocol_str_lookup[class_name]
@@ -53,11 +51,11 @@ class Parser:
             return res
 
     @lru_cache
-    def _reverse_protocols_str_lookup(self):
+    def _reverse_protocols_str_lookup(self) -> Dict[Any, int]:
 
         return {v: k for k, v in self.__protocol_str_lookup.items()}
 
-    def get_protocol_name_by_class(self, cls):
+    def get_protocol_name_by_class(self, cls: Any) -> str:
         """ return name of class"""
         # inefficient,only use to ensure the latest version is use cach
         rev_protocol_str = self._reverse_protocols_str_lookup()
@@ -69,7 +67,7 @@ class Parser:
         else:
             return res
 
-    def parse(self, layer, identifier, raw_bytes):
+    def parse(self, layer: int, identifier: int, raw_bytes: bytes):
         """ use to register parser"""
         try:
             res = self.__protocol_parsers[layer][identifier](raw_bytes)
@@ -88,4 +86,4 @@ class Parser:
             return res
 
 
-Protocol_Parser = Parser()
+Protocol_Parser: Parser = Parser()
