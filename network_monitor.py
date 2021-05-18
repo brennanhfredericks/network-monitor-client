b40adf2e1f3f93ea3372baa209a62e8408179cd2
Brennan Fredericks <brennanhfredericks@gmail.com>
2021-05-18 11:28:52 +0200

    moved information args to network_monitor

--- network_monitor.py
+++ network_monitor.py
@@ -1,4 +1,73 @@
-from network_monitor import args_parser
+import netifaces
+import argparse
+from network_monitor import generate_configuration_template
+
+# configure start up manager
+
+
+def main(args: argparse.Namespace) -> int:
+
+    # first do info args with with starting application
+    if args.list_gateways:
+        print("gateways: ")
+        for k, v in netifaces.gateways().items():
+            print(f"\taddress family: {k}, interface: {v}")
+
+    if args.list_interfaces:
+        print(f"interfaces: {netifaces.interfaces()}")
+
+    if args.generate_config_file:
+        generate_configuration_template(args.generate_config_file)
+        print(f"created configuration file: {args.generate_config_file}")
+
+
+def args_parser() -> int:
+
+    basic_parser = argparse.ArgumentParser(
+        description="monitor ethernet network packets.", add_help=True
+    )
+
+    # add arguments
+    basic_parser.add_argument(
+        "-i",
+        "--interface",
+        action="store",
+        choices=netifaces.interfaces(),
+        type=str,
+        help="specify which interface to monitor",
+    )
+    basic_parser.add_argument(
+        "-li",
+        "--list-interfaces",
+        action="store_true",
+        help="list all available interfaces",
+    )
+    basic_parser.add_argument(
+        "-lg",
+        "--list-gateways",
+        action="store_true",
+        help="list all available gateways",
+    )
+    basic_parser.add_argument(
+        "-gcf",
+        "--generate-config-file",
+        action="store",
+        type=str,
+        help="generate a configuration template file",
+    )
+    basic_parser.add_argument(
+        "-lcf",
+        "--load-config-file",
+        action="store",
+        type=str,
+        help="load a configuration file",
+    )
+
+    # parse arguments
+    args: argparse.Namespace = basic_parser.parse_args()
+
+    return main(args)
+
 
 if __name__ == "__main__":
 
